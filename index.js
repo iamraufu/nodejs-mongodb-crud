eeee75741c3336111e96278b76d4b438fa3b5c86
iamraufu <raufu.dev@gmail.com>
2021-11-13 02:30:23 +0600

    [ADD] final code

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const password = 'iXYS6zJhx$8PUCp';
+
+const uri = "mongodb+srv://vegetableCEO:iXYS6zJhx$8PUCp@cluster0.ohxhr.mongodb.net/vegetableShop?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+client.connect(err => {
+    const productCollection = client.db("vegetableShop").collection("products");
+
+    app.get('/products', (req, res) => {
+        productCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/product/:id', (req, res) => {
+        productCollection.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    })
+
+    app.post("/addProduct", (req, res) => {
+        const product = req.body;
+        productCollection.insertOne(product)
+            .then(result => {
+                res.redirect('/')
+            })
+    })
+
+    app.patch('/update/:id', (req, res) => {
+        productCollection.updateOne({ _id: ObjectId(req.params.id) }, {
+                $set: { name: req.body.name, price: req.body.price, quantity: req.body.quantity }
+            })
+            .then(result => {
+                res.send(result.modifiedCount > 0)
+            })
+    })
+
+    app.delete('/delete/:id', (req, res) => {
+        productCollection.deleteOne({ _id: ObjectId(req.params.id) })
+        .then(result=>{
+            res.send(result.deletedCount > 0)
+        })
+    })
+});
+
+app.listen(process.env.PORT || 3000);
